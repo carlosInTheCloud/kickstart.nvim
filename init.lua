ead05c83b52e8db5e228c0f5b52ae7582ea0b289
Carlos Espinosa <carlos.espinosa@wbd.com>
2024-11-08 12:21:14 -0500

    New file for short keys (wip) and added markdown lsp

--- init.lua
+++ init.lua
@@ -651,14 +651,15 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         pyright = {},
+        marksman = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        ts_ls = {},
         --
 
         lua_ls = {
