6a7aac561c8ae632388df50a2112724ab2acba7c
Carlos Espinosa <carlos.espinosa@wbd.com>
2024-12-11 12:20:56 -0500

    feat: map <leader>bb to Telescope buffers

--- init.lua
+++ init.lua
@@ -480,6 +480,12 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
+      vim.keymap.set('n', '<leader>bb', function()
+        builtin.buffers {
+          prompt_title = 'List Open Buffers',
+        }
+      end, { desc = 'Open Buffers' })
     end,
   },
 
