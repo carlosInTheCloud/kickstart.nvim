b1473f384ca389547049b73ef1083dc94f156fcf
Carlos Espinosa <carlos.espinosa@wbd.com>
2025-02-16 10:57:14 -0500

    feat: displaying hidden files in neotree

--- init.lua
+++ init.lua
@@ -270,6 +270,15 @@ require('lazy').setup({
       'MunifTanjim/nui.nvim',
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dofiles = false,
+          visible = true,
+          hide_gitignored = false,
+        },
+      },
+    },
   },
   {
     'iamcco/markdown-preview.nvim',
